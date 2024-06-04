a96a016cd1fc9c3010d8aba1139c8a45ebe38c0f
ShaziaMahar <shaziamahar478@gmail.com>
2024-06-04 12:12:21 +0500

    commit7

---
+++ class7.js
@@ -0,0 +1,83 @@
+// let num = [1,2,,3,4,5,6,7,8,9];
+// for in loop
+// how many
+
+// // let name = []
+// // for(let i=1; i < 100; i++){
+//     name.push("hello")
+// }
+// console.log(name);
+ 
+
+// let name = ["shazia","hadeesa", "hadia", "sawera"];
+// for(let elem in name){
+//     console.log(elem );
+// }
+
+
+// let name = ["shazia","hadeesa", "hadia", "sawera"];
+// for (let elem of name){
+//     console.log(elem);
+//  }
+
+// let arr = ["shazia","hadeesa", "hadia", "sawera"];
+// arr.forEach(function(val, ind){
+// console.log(ind , val);
+// })
+
+
+// let arr = ["shazia","hadeesa", "hadia", "sawera"];
+// arr.forEach(function(val, index){
+// console.log( val , index);
+// })
+
+
+
+// array filter
+// let filterarr = [1,2,3,4,5,6,7,8,9,10]
+// let arr = filterarr.filter( function(val){
+// if(val & 2 != 0){
+//     return true;
+// }else{
+//     return false
+// }
+// })
+// console.log(arr);
+// console.log(filterarr);
+
+
+// let filterarr = [1,2,3,4,5,6,7,8,9,10]
+// let arr = filterarr.filter( function(val){
+// if(val % 2 === 0){
+//     return true;
+// }else{
+//     return false
+// }
+// })
+// console.log(arr);
+// console.log(filterarr);
+
+
+
+//sort
+// reepeated value remove
+// let arr = [0,0,0, 1,3, 0,4,6,7,8,9,9,0,0,9,8,7,1,1,1,1,2,3,3,4,5,6];
+// console.log(arr);
+// let arrost = arr.sort(function(a,b){
+//     if(a > b){
+//         return 1
+//     }else{
+//         return -1
+//     }
+// })
+// console.log(arr);
+
+
+
+//previous  current
+// let arr =["ali", " ","ahmad"];
+// let a = arr.reduce(function(preval, currval){
+//     return preval + currval;
+// })
+// console.log(a);
+
